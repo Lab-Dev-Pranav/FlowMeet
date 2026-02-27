184bc56c9b16840a6093088cf19bf930d4ee2289
Pranav Patil <pranavpatilg2004@gmail.com>
2026-02-27 17:18:40 +0530

    env.js info edited

--- Frontend/src/environment.js
+++ Frontend/src/environment.js
@@ -1,5 +1,8 @@
 
-let Is_Prod_Or_Dev = true;
+let Is_Prod_Or_Dev = false;
+
+// false for Dev 
+// true for Prod
 
 const server = Is_Prod_Or_Dev ? "https://flowmeet-be.onrender.com" : "http://localhost:3000" 
  
